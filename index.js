16060954f166d0ad41caf7bd0bc9f630fd2dc76d
ajanaku <mykdahunsi@gmail.com>
2023-10-05 01:26:41 +0100

    jS added

---
+++ index.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+    
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+    
+    const menuBtn = document.querySelector('.dropbtn');
+    const dropdown = document.querySelector('.dropdown-content');
+
+    const header = document.querySelector('header');
+
+    menuBtn.addEventListener('click', (e) => {
+        e.preventDefault(); // Prevent the default link behavior
+        dropdown.classList.toggle('active');
+    });
+
+    // Close the dropdown when clicking outside of the header
+    document.addEventListener('click', (e) => {
+        if (!header.contains(e.target) && e.target !== menuBtn) {
+            dropdown.classList.remove('active');
+        }
+    });
+    
+    menuBtn.addEventListener('click', () => {
+        dropdown.classList.toggle('active');
+    });
+});
