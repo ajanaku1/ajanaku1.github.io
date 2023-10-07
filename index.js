2d3c3e35f2635ddbbabd51d2b80e1a32408ff60c
ajanaku <mykdahunsi@gmail.com>
2023-10-07 20:25:15 +0100

    nb 2nd trail

--- index.js
+++ index.js
@@ -8,24 +8,21 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     });
-    
-    const menuBtn = document.querySelector('.dropbtn');
-    const dropdown = document.querySelector('.dropdown-content');
-    const header = document.querySelector('header');
 
-    menuBtn.addEventListener('click', (e) => {
-        e.preventDefault(); // Prevent the default link behavior
-        dropdown.classList.toggle('active');
-    });
-
-    // Close the dropdown when clicking outside of the header
-    document.addEventListener('click', (e) => {
-        if (!header.contains(e.target) && e.target !== menuBtn) {
-            dropdown.classList.remove('active');
-        }
-    });
+    document.addEventListener('DOMContentLoaded', function() {
+        const menuBtn = document.querySelector('.dropbtn');
+        const dropdown = document.querySelector('.dropdown-content');
     
-    menuBtn.addEventListener('click', () => {
-        dropdown.classList.toggle('active');
+        menuBtn.addEventListener('click', function(e) {
+            e.preventDefault(); // Prevent the default link behavior
+            dropdown.classList.toggle('active');
+        });
+    
+        // Close the dropdown when clicking outside of the header
+        document.addEventListener('click', function(e) {
+            if (!dropdown.contains(e.target) && e.target !== menuBtn) {
+                dropdown.classList.remove('active');
+            }
+        });
     });
 });
