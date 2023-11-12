c588670eb9d6c89fc8267b917284f3e0c2750916
ajanaku <mykdahunsi@gmail.com>
2023-11-12 16:47:00 +0100

    about page now about section

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ document.addEventListener('DOMContentLoaded', function() {
         const menuBtn = document.querySelector('.dropbtn');
         const dropdown = document.querySelector('.dropdown-content');
     
-        menuBtn.addEventListener('click', function(e) {
-            e.preventDefault(); // Prevent the default link behavior
+        menuBtn.addEventListener('click', function (e) {
+            e.preventDefault(); //! Prevent the default link behavior
             dropdown.classList.toggle('active');
         });
     
-        // Close the dropdown when clicking outside of the header
-        document.addEventListener('click', function(e) {
+        //! Close the dropdown when clicking outside of the header
+        document.addEventListener('click', function (e) {
             if (!dropdown.contains(e.target) && e.target !== menuBtn) {
                 dropdown.classList.remove('active');
             }
