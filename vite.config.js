04c3e4d1e542302d643c6caefb4915e29e5197b9
ajanaku <mykdahunsi@gmail.com>
2024-01-12 00:02:49 +0100

    wm

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ajanaku1.github.io/",
+  base: "/",
   plugins: [react()],
   assetsInclude: ["**/*.JPG"],
   build: {
