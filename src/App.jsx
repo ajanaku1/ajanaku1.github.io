219dd2ef9edad8c3ab59f09e0e88ab8ea37be513
ajanaku <mykdahunsi@gmail.com>
2024-01-12 10:10:57 +0100

    favicon

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <HomeSec />
       <About />
       <Portfolio />
       <Contact />
       <Footer />
-    </div>
+      <SpeedInsights />
+    </>
   );
 }
 
